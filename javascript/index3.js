275c705995e4e04c5fa3b05ab271c751f9206e75
vishakhadhamgunde <vishakhadhamgunde1.2.3.v@gmail.com>
2019-01-26 00:00:05 -0500

    js file is modified amd added

---
+++ javascript/index3.js
@@ -0,0 +1,19 @@
+
+var person = {
+    firstName: 'Chandrashekhar',
+    lastName: 'Dhamgunde',
+    age: 22,
+    daughter:{
+        name:'Mohini',
+        age:25
+    },
+    myfunc:function(daughterName,fatherName,daughterAge) {
+        console.log(par.daughterName + 'is a daughter of' + fatherName + 'and she is' 
+        + par.daughterAge + 'years old' )
+    }
+};
+console.log(person);
+console.log(person.daughter.Name);
+ 
+person.myfunc(person.daughter.name,person.firstName,
+person.daughter.age);
