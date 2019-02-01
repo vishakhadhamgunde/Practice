f65e9d5154b909542a02ccce68ca1bb52bc8d63c
vishakhadhamgunde <vishakhadhamgunde1.2.3.v@gmail.com>
2019-01-31 21:23:50 -0500

    javascript file

---
+++ javascript/index7.js
@@ -0,0 +1,11 @@
+var girl={
+    firstName:'Vishakha',
+    lastName:'Dhamgunde',
+    height:4.10,
+    weight:47,
+    education:BE(T.E),
+    getFullname :function(){
+        console.log(this === per);
+        console.log(this.firstName+' '+this.lastName);
+    }
+}
